4584ac75529f51ae02391723d5b135d787cbaee1
Felipe <fe.ferradap@duocuc.cl>
2024-06-29 22:28:45 -0400

    Contenedores dinamicas en main

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector(".wrapper").classList.toggle("side-panel-open");
     });
 });
+
+function refreshPage() {
+    location.reload();
+}
+
+function togglePanel(panelId) {
+    const panel = document.getElementById(panelId);
+    panel.classList.toggle('show');
+}
